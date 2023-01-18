477066bd74838bce1a3775cf9679565fafc19177
Ines Achour <inesachour@insat.u-carthage.tn>
2023-01-18 18:55:41 +0100

    calender fixes

--- src/calendar/services/task.service.ts
+++ src/calendar/services/task.service.ts
@@ -1,30 +1,35 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { TaskEntity } from "../entities/task.entity";
-import { Repository } from "typeorm";
-import { CreateTaskDto } from "../dto/create-task.dto";
-import { UpdateTaskDto } from "../dto/update-task.dto";
-import { User } from "../../user/entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskEntity } from '../entities/task.entity';
+import { Repository } from 'typeorm';
+import { CreateTaskDto } from '../dto/create-task.dto';
+import { UpdateTaskDto } from '../dto/update-task.dto';
+import { User } from '../../user/entities/user.entity';
 
 @Injectable()
 export class TaskService {
+  constructor(
+    @InjectRepository(TaskEntity)
+    private readonly taskRepository: Repository<TaskEntity>,
+  ) {}
 
-  constructor(@InjectRepository(TaskEntity) private readonly taskRepository:Repository<TaskEntity>) {  }
-
-  async find(user:number) {
-    return await this.taskRepository.find()
+  async find(user: number, date: string) {
+    return this.taskRepository
+      .createQueryBuilder('task')
+      .where('task.userId = :user', { user: user })
+      .andWhere('task.date = :date', { date: date })
+      .getMany();
   }
 
-  async addTask(task:CreateTaskDto){
-    return await this.taskRepository.save(task)
+  async addTask(task: CreateTaskDto) {
+    return await this.taskRepository.save(task);
   }
 
-  async updateTask(id:number, task:UpdateTaskDto){
-    return await this.taskRepository.update({id:id},task)
+  async updateTask(id: number, task: UpdateTaskDto) {
+    return await this.taskRepository.update({ id: id }, task);
   }
 
-  async deleteTask(id:number){
-    return await this.taskRepository.delete(id)
+  async deleteTask(id: number) {
+    return await this.taskRepository.delete(id);
   }
-
 }
